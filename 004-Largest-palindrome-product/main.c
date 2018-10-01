352f7793e99866082d4aeebc604084250db87680
darkdan21 <dan_j@live.co.uk>
2018-10-01 21:55:09 +0100

    Change the config so the correct solution is actually reached

--- 004-Largest-palindrome-product/main.c
+++ 004-Largest-palindrome-product/main.c
@@ -7,8 +7,8 @@ int main(int argc, char** argv){
     printf("Problem: 4\n");
     long long total = 0;
     long long max = 0;
-    for(long long i = 100; i<=99999; i++){
-        for(long long ii=100; ii<=99999; ii++){
+    for(long long i = 100; i<=999; i++){
+        for(long long ii=100; ii<=999; ii++){
            total = i*ii;
            if(ispalindrome(total) == 0){
                if(total > max){
